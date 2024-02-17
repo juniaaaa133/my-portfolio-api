d384d70dd33eaf6baa0f199f252f689dedf159ea
Reinnn <xopeofhopeness2004@gmail.com>
2024-02-17 15:02:23 +0630

    Initial Commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return [
+            Test::all()
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    return Response(Test::create($request->all()),201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Test $test)
+    {
+        //
+        return Response($test);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Test $test)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Test $test)
+    {
+        //
+        $test->delete();
+        return Response()->noContent();
+    }
+}
