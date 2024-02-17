cd1199b8d6970a422552b905f6ca88f1cef2cefd
Reinnn <xopeofhopeness2004@gmail.com>
2024-02-17 17:15:57 +0630

    Debug

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::apiResource('tests',TestController::class);
+Route::resource('posts',PostController::class);
